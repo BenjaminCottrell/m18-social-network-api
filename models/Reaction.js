b6d75a98c3b7094c459d491da91b38c7e00e53d3
BenCottrell <bencottrell48@yahoo.com>
2022-12-12 21:19:03 -0500

    renamed Course model to Reaction and edited Reaction model. Also deleted unneeded code from server.js

---
+++ models/Reaction.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a reaction model
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (date) => {
+        return formatDate(date)
+      },
+      username: {
+        type: String,
+        required: true,
+      }
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const Reaction = model('reaction', reactionSchema);
+
+module.exports = Reaction;

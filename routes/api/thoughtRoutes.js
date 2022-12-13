fc632a65ea84d7f770fce99b933e0e9d0f99fbdf
BenCottrell <bencottrell48@yahoo.com>
2022-12-12 21:57:00 -0500

    added routes for reactions to thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,6 +5,8 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  addReaction,
+  deleteReaction,
 } = require('../../controllers/thoughtController.js');
 
 // /api/thoughts
@@ -17,4 +19,10 @@ router
   .put(updateThought)
   .delete(deleteThought);
 
+router.route('/thoughtId/reactions')
+  .post(addReaction);
+
+router.route('thoughtId/reactions/:reactionId')  
+  .delete(deleteReaction);
+  
 module.exports = router;
